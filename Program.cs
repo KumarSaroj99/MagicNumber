d72b87574e013e6d513690f6543305d3e046a56e
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-07-11 23:47:08 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius of circle : ");
+            int radius = Convert.ToInt32(Console.ReadLine());
+
+            float pi = 3.14f;            //magic number
+            Console.WriteLine("Area of circle is : "+2*pi*radius);
+        }
+    }
+}
